3eed9b61dd428016b821b27f65974c09138c1d7d
Juliya Khanenko <jullietnm@gmail.com>
2022-12-02 20:47:41 +0200

    !!contacts.lengt &&

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,14 @@
 import { ContactForm } from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
-// import initialContacts from './ContactList/initialContacts.json';
+import initialContacts from './ContactList/initialContacts.json';
 import Filter from './Filter/Filter';
 
 import { Wrapper, Title, Title2, Title3 } from './App.styled';
-// import { useSelector } from 'react-redux';
-// import { getContacts } from 'redux/selectors';
+import { useSelector } from 'react-redux';
+import { getContacts } from 'redux/selectors';
 
 export default function App() {
-  // const contacts = useSelector(getContacts); /// рендер пошуку якщо є данні
+  const contacts = useSelector(getContacts); /// рендер пошуку якщо є данні
   return (
     <>
       <Wrapper>
@@ -17,13 +17,12 @@ export default function App() {
         <Title2>Contacts</Title2>
         <Title3>Find contact by name</Title3>
 
-        {/* {!!contacts.lengt && ( /// рендер пошуку якщо є данні
+        {!!contacts.lengt && ( /// рендер пошуку якщо є данні
           <>
-           
+            <Filter />
+            <ContactList />
           </>
-        )} */}
-        <Filter />
-        <ContactList />
+        )}
       </Wrapper>
     </>
   );
