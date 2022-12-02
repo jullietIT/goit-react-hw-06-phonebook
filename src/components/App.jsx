acb86386dffb91e2f718aa80c6f127d5853b5936
Juliya Khanenko <jullietnm@gmail.com>
2022-12-02 20:52:51 +0200

    contacts.lengt &&

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import { ContactForm } from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
-import initialContacts from './ContactList/initialContacts.json';
+// import initialContacts from './ContactList/initialContacts.json';
 import Filter from './Filter/Filter';
 
 import { Wrapper, Title, Title2, Title3 } from './App.styled';
